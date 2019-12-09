19f3b2086d4273e47e6f2bdf476702879b8f7a4b
Jielun Chen <chenjielunn@gmail.com>
2019-12-10 07:44:56 +0800

    finished models and started on controller for Yelp MVP

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,37 +1,29 @@
-class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show, :edit]
-
+clclass RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
 
   def show
     @restaurant = Restaurant.find(params[:id])
-    @reviews = @restaurant.reviews
+    @review = Review.new
   end
 
   def new
     @restaurant = Restaurant.new
   end
 
-  def edit
-    @restaurant = Restaurant.find(params[:id])
-  end
-
   def create
     @restaurant = Restaurant.new(restaurant_params)
-      if @restaurant.save
-        redirect_to restaurant_path(@restaurant)
-      else
-        render :new
-      end
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render "new"
+    end
   end
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
-  end
+  private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :category)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
